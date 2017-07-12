7aee81b48860f79cf06b1e4cbc5334f624528b7f
Edwin Marte Torres <emarte@intellisys.com.do>
2017-07-12 08:32:22 -0400

    Ignoring Coverage Test Files and Making Test Command to work

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -3,7 +3,7 @@
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
 
-var webpackConfig = require('../../../testing/build/webpack.test.conf')
+var webpackConfig = require('../../../tribe-kitchen/build/webpack.test.conf')
 
 module.exports = function (config) {
   config.set({
